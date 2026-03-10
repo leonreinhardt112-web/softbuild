ce383884d3cde623c60bef8f40627fa0ae034cd1
Leon Reinhardt <leon.reinhardt112@gmail.com>
2026-03-10 10:29:42 +0000

    File changes

--- src/components/kalkulation/AngebotImportDialog.jsx
+++ src/components/kalkulation/AngebotImportDialog.jsx
@@ -129,7 +129,7 @@ export default function AngebotImportDialog({ project, kalkulation, onPositionen
           id: crypto.randomUUID(),
           name: ap.kurztext_angebot,
           beschreibung: `Angebot ${lieferantName}`,
-          kostentyp: "NU",
+          kostentyp: "Material",
           menge: 1,
           einheit: ap.einheit || lvPos.unit || "pauschal",
           kosten_einheit: ap.ep
