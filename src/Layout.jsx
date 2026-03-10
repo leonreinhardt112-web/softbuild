795d26948a47ae338900bb2bc3a9b3d721dea5a1
Leon Reinhardt <leon.reinhardt112@gmail.com>
2026-03-10 21:57:17 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
+import { useUnsavedChanges } from "@/components/common/UnsavedChangesContext";
 
 import {
   LayoutDashboard,
